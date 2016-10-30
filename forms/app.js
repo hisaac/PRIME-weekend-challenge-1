4866dd0bc9abf83b1ea6637ad4f48a9ac977401c
Isaac Halvorson <hello@hisaac.net>
2016-10-30 12:33:50 -0500

    create empty object for employee info

--- forms/app.js
+++ forms/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    console.log('ready to rock');
+    var employeeInfo = {
+        firstName: 'N/A',
+        lastName: 'N/A',
+        idNumber: NaN,
+        jobTitle: 'N/A',
+        annualSalary: NaN,
+    };
+    console.log(employeeInfo);
   });
